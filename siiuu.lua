e86bfa175f8790556593b62331d34cf139bb0165
Rowouxik <132126032+Rowouxik@users.noreply.github.com>
2023-05-20 11:35:33 +0300

    Update siiuu.lua

--- siiuu.lua
+++ siiuu.lua
@@ -714,34 +714,6 @@ end)
 
 local mod = main:Button("Aim",function()
 		
-local function ezEsp(player)
-    local esp = Instance.new("Highlight", player)
-    wait(1)
-    if player:FindFirstChild("friendly_marker") then
-        esp.FillColor = Color3.new(0, 0, 1)
-    end
-end
-
-local function Lesp(player)
-    for _, shit in pairs(player:GetChildren()) do
-        if shit.Name == "Hightlight" then
-            shit:Destroy()
-        end
-    end
-end
-
-for _, player in pairs(game.Workspace:GetChildren()) do
-    if player.Name == "soldier_model" then
-        Lesp(player)
-        ezEsp(player)
-    end
-end
-
-game.Workspace.DescendantAdded:Connect(function(player)
-    if player.Name == "soldier_model" then
-        Lesp(player)
-        spawn(function()ezEsp(player)end)
-    end
-end)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mopsfl/rbxmopshub/main/loader.lua"))()
 		
 end)
